29ed7ca58458cefab4ce2a686102979c05416a93
Shi Hao Huang <72808191+ShiHaoHuang23@users.noreply.github.com>
2020-11-29 20:31:46 +0800

    Add files via upload

---
+++ find ptt title.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'http://www.ptt.cc/bbs/Gossiping/index.html'
+session = requests.Session()
+payload = {"from": "/bbs/Gossiping/index.html",
+            "yes": "yes"}
+
+session.post("http://www.ptt.cc/ask/over18?from=%2Fbbs%2FGossiping%2Findex.html",payload)
+
+html = session.get(url).text
+soup = BeautifulSoup(html,'html.parser')
+titles = soup.select('.title')
+
+for title in titles:
+    print(title.a.text)
+    print(title.a['href'])
+    
