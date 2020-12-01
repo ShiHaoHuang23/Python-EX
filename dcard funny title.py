8c5f270f2e3c82d849a867be519f91bb9719de45
Shi Hao Huang <72808191+ShiHaoHuang23@users.noreply.github.com>
2020-12-01 22:17:26 +0800

    Add files via upload

---
+++ dcard funny title.py
@@ -0,0 +1,9 @@
+import json
+import requests
+
+api_url = "http://www.dcard.tw/_api/forums/funny/posts?limit=100"
+res = requests.get(api_url).text
+data = json.loads(res)
+
+for post in data:
+    print(post["title"])
